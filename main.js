e9e3859b8ccff567de9829113b04f7223b0f5298
Matthieu <matthieu@Matthieus-MacBook-Pro.local>
2017-03-15 21:13:26 +0100

    callr callback

--- main.js
+++ main.js
@@ -1,6 +1,28 @@
-var path = require('path');
+var callr = require('callr');
 var express = require('express');
+var path = require('path');
+var dotenv = require('dotenv');
+
+dotenv.load({ silent: true });
 var app = express();
+
+var server = app.listen(8080, function () {
+   var host = server.address().address
+   var port = server.address().port
+
+   console.log("Flower is listening at http://%s:%s", host, port)
+});
+
+var api = new callr.api(process.env.CALLR_ID, process.env.CALLR_PWD);   
+
+var type = 'sms.mo';
+var endpoint = 'https://flowerserver-arflower.44fs.preview.openshiftapps.com/sms_webhook';
+var options = null;
+
+api.call('webhooks.subscribe', type, endpoint, options).success(function(response) {
+    // success callback
+});
+
 
 app.use('/third_party', express.static(__dirname + '/third_party'));
 app.use('/data', express.static(__dirname + '/data'));
@@ -13,6 +35,8 @@ app.get('/health_check', function (req, res) {
    res.send('alive');
 });
 
-var server = app.listen(8080, function () {
-   console.log("Flower is listening at http://%s:%s", host, port)
+app.get('/sms_webhook', function (req, res) {
+   console.log('sms recieved', req)
+   res.sendFile(path.join(__dirname, 'flower.html'));
 });
+
